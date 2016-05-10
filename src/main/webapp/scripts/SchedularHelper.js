c926448d31842ee585ad3c074bd791792c92fd99
nizar.ben.mansour@gmail.com <ED5Dy6dg2kx7>
2016-05-10 16:40:37 +0400

    sadas

---
+++ src/main/webapp/scripts/SchedularHelper.js
@@ -0,0 +1,21 @@
+SchedularCalendarApp
+  .module('mwl.calendar.docs')
+  .factory('alert', function($uibModal) {
+
+    function show(action, event) {
+      return $uibModal.open({
+        templateUrl: 'SchedularModalContent.jsp',
+        controller: function() {
+          var vm = this;
+          vm.action = action;
+          vm.event = event;
+        },
+        controllerAs: 'vm'
+      });
+    }
+
+    return {
+      show: show
+    };
+
+  });
