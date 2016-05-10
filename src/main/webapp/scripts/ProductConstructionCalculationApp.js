5f43a73f1fff363b9e3020cbb3e1c79abd369bcb
nizar.ben.mansour@gmail.com <ED5Dy6dg2kx7>
2016-05-10 18:43:59 +0400

    fdsf

---
+++ src/main/webapp/scripts/ProductConstructionCalculationApp.js
@@ -0,0 +1,7 @@
+'use strict';
+var ProductConstructionCalculationApp = angular.module('myApp','ProductConstructionApp.js','ProductConstructionCalculationApp', ['mwl.calendar', 'ngCookies',
+		  'ui.bootstrap','ngAnimate'
+		 ]);
+
+
+
